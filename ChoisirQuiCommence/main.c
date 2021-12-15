4112fdb1e1f7c8af30fd6305dc983593d0b8b53b
mathieuaubert2601 <mathieu.aubert@edu.ece.fr>
2021-12-15 08:59:46 +0100

    ChoisirQuiCommence Version1.2 15122021 Mathieu

--- ChoisirQuiCommence/main.c
+++ ChoisirQuiCommence/main.c
@@ -24,33 +24,46 @@ void choisirQuiCommence(joueur_t ordreJoueur[],joueur_t tabJoueur[], int nbrJoue
 {
     ///Déclaration des variables
     int quiCommence = rand()%nbrJoueur + 1;
-    for(int i = 0; i<nbrJoueur ; i++)
+
+
+    for(int i = 0; i<nbrJoueur-1 ; i++)
     {
         if(tabJoueur[i].nomJoueur == quiCommence)
         {
             ordreJoueur[0] = tabJoueur[i];
         }
     }
-    srand(time(NULL));
-    do
-    {
-        int aLaGauche;
-        printf("Qui est le joueur à la gauche de joueur %d :\n-%s : 1\n-%s : 2",quiCommence);
-        fflush(stdin);
-        scanf("%d",&aLaGauche);
-    }while(aLaGauche == quiCommence);
+    if(nbrJoueur == 2)
+        {
+            if(quiCommence == 1)
+            {
+
+                ordreJoueur[1] = tabJoueur[1];
+                printf("La personne qui jouera apres %s est %s \n",ordreJoueur[0].nomJoueur , ordreJoueur[1].nomJoueur);
+            }
+            if(quiCommence == 2)
+            {
+                ordreJoueur[1] = tabJoueur[0];
+                printf("La personne qui jouera apres %s est %s \n",ordreJoueur[1].nomJoueur, ordreJoueur[0].nomJoueur);
+            }
+        }
 
 }
 
 int main()
 {
 
+    srand(time(NULL));
     joueur_t joueur1 = {"Mathieu",1};
     joueur_t joueur2 = {"Thibault",2};
     joueur_t joueur3 = {"Adrien",3};
     joueur_t joueur4 = {"Philippe",4};
-    int nombreJoueur = 4;
+    int nombreJoueur = 2;
 
     joueur_t tableau_Joueur[4] = {joueur1, joueur2, joueur3, joueur4};
+    joueur_t ordreJeux[4];
+
+    choisirQuiCommence(ordreJeux, tableau_Joueur, nombreJoueur);
+
     return 0;
 }
