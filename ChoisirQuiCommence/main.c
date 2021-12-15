cad22f5bb27a2f364b0f079122f3465fe06fafe5
mathieuaubert2601 <mathieu.aubert@edu.ece.fr>
2021-12-15 18:11:18 +0100

    ChoisirQuiCommence Version2 15122021 Mathieu

--- ChoisirQuiCommence/main.c
+++ ChoisirQuiCommence/main.c
@@ -26,9 +26,9 @@ void choisirQuiCommence(joueur_t ordreJoueur[],joueur_t tabJoueur[], int nbrJoue
     int quiCommence = rand()%nbrJoueur + 1;
 
 
-    for(int i = 0; i<nbrJoueur-1 ; i++)
+    for(int i = 0; i<nbrJoueur ; i++)
     {
-        if(tabJoueur[i].nomJoueur == quiCommence)
+        if(tabJoueur[i].numeroJoueur == quiCommence)
         {
             ordreJoueur[0] = tabJoueur[i];
         }
@@ -39,14 +39,78 @@ void choisirQuiCommence(joueur_t ordreJoueur[],joueur_t tabJoueur[], int nbrJoue
             {
 
                 ordreJoueur[1] = tabJoueur[1];
+
                 printf("La personne qui jouera apres %s est %s \n",ordreJoueur[0].nomJoueur , ordreJoueur[1].nomJoueur);
             }
             if(quiCommence == 2)
             {
                 ordreJoueur[1] = tabJoueur[0];
-                printf("La personne qui jouera apres %s est %s \n",ordreJoueur[1].nomJoueur, ordreJoueur[0].nomJoueur);
+                printf("La personne qui jouera apres %s est %s \n",ordreJoueur[0].nomJoueur, ordreJoueur[1].nomJoueur);
             }
         }
+    if(nbrJoueur == 3)
+    {
+            if(quiCommence == 1)
+            {
+
+                ordreJoueur[1] = tabJoueur[1];
+                ordreJoueur[2] = tabJoueur[2];
+
+                printf("La personne qui jouera apres %s est %s puis %s \n",ordreJoueur[0].nomJoueur , ordreJoueur[1].nomJoueur, ordreJoueur[2].nomJoueur);
+            }
+            if(quiCommence == 2)
+            {
+                ordreJoueur[1] = tabJoueur[2];
+                ordreJoueur[2] = tabJoueur[0];
+                printf("La personne qui jouera apres %s est %s puis %s \n",ordreJoueur[0].nomJoueur , ordreJoueur[1].nomJoueur, ordreJoueur[2].nomJoueur);
+            }
+            if(quiCommence == 3)
+            {
+                ordreJoueur[1] = tabJoueur[0];
+                ordreJoueur[2] = tabJoueur[1];
+                printf("La personne qui jouera apres %s est %s puis %s \n",ordreJoueur[0].nomJoueur , ordreJoueur[1].nomJoueur, ordreJoueur[2].nomJoueur);
+            }
+
+    }
+    if(nbrJoueur == 4)
+    {
+            if(quiCommence == 1)
+            {
+
+                ordreJoueur[1] = tabJoueur[1];
+                ordreJoueur[2] = tabJoueur[2];
+                ordreJoueur[3] = tabJoueur[3];
+
+                printf("La personne qui jouera apres %s est %s puis %s et enfin %s\n",ordreJoueur[0].nomJoueur , ordreJoueur[1].nomJoueur, ordreJoueur[2].nomJoueur, ordreJoueur[3].nomJoueur);
+                printf("Veuillez vous placer dans l'ordre dans le sens horaire \n");
+            }
+            if(quiCommence == 2)
+            {
+                ordreJoueur[1] = tabJoueur[2];
+                ordreJoueur[2] = tabJoueur[3];
+                ordreJoueur[3] = tabJoueur[0];
+                printf("La personne qui jouera apres %s est %s puis %s et enfin %s\n",ordreJoueur[0].nomJoueur , ordreJoueur[1].nomJoueur, ordreJoueur[2].nomJoueur, ordreJoueur[3].nomJoueur);
+                printf("Veuillez vous placer dans l'ordre dans le sens horaire \n");
+            }
+            if(quiCommence == 3)
+            {
+                ordreJoueur[1] = tabJoueur[3];
+                ordreJoueur[2] = tabJoueur[0];
+                ordreJoueur[3] = tabJoueur[1];
+                printf("La personne qui jouera apres %s est %s puis %s et enfin %s\n",ordreJoueur[0].nomJoueur , ordreJoueur[1].nomJoueur, ordreJoueur[2].nomJoueur, ordreJoueur[3].nomJoueur);
+                printf("Veuillez vous placer dans l'ordre dans le sens horaire \n");
+            }
+            if(quiCommence == 4)
+            {
+                ordreJoueur[1] = tabJoueur[0];
+                ordreJoueur[2] = tabJoueur[1];
+                ordreJoueur[3] = tabJoueur[2];
+                printf("La personne qui jouera apres %s est %s puis %s et enfin %s\n",ordreJoueur[0].nomJoueur , ordreJoueur[1].nomJoueur, ordreJoueur[2].nomJoueur, ordreJoueur[3].nomJoueur);
+                printf("Veuillez vous placer dans l'ordre dans le sens horaire \n");
+
+            }
+
+    }
 
 }
 
@@ -58,7 +122,7 @@ int main()
     joueur_t joueur2 = {"Thibault",2};
     joueur_t joueur3 = {"Adrien",3};
     joueur_t joueur4 = {"Philippe",4};
-    int nombreJoueur = 2;
+    int nombreJoueur = 4;
 
     joueur_t tableau_Joueur[4] = {joueur1, joueur2, joueur3, joueur4};
     joueur_t ordreJeux[4];
