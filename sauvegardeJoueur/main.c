b5fc8d1438f06f60628af05c21ef2610be6667e2
mathieuaubert2601 <mathieu.aubert@edu.ece.fr>
2021-12-17 09:55:29 +0100

    SauvegardeChargementJoueur Version 2 17122021 Mathieu

--- sauvegardeJoueur/main.c
+++ sauvegardeJoueur/main.c
@@ -64,8 +64,11 @@ void chargerNombreJoueur(int* nombre_De_joueur)
 
 }
 
+//////////////////////////////////////////////////////////
+////////Sous programme pour saubegarder 2 de joueurs /////
+//////////////////////////////////////////////////////////
 
-void sauvegardeJoueur(joueur_t joueurA)
+void sauvegardeDeuxJoueur(joueur_t joueurA, joueur_t joueurB)
 {
     ///Ouverture et test d'ouverture du fichier
     FILE* donneJoueur = fopen("fichiersSauvegarde/joueurA.bin", "wb+");
@@ -76,13 +79,17 @@ void sauvegardeJoueur(joueur_t joueurA)
     }
 
     fwrite(&joueurA, sizeof(joueur_t),1,donneJoueur);
+    fwrite(&joueurB, sizeof(joueur_t),1,donneJoueur);
 
     ///Fermeture du fichier
     fclose(donneJoueur);
     donneJoueur = NULL;
 }
 
-void chargerJoueur(joueur_t* joueurAcharger)
+//////////////////////////////////////////////////////////
+////////Sous programme pour charger 2 de joueurs /////////
+//////////////////////////////////////////////////////////
+void chargerDeuxJoueur(joueur_t* joueurAchargerA, joueur_t* joueurAchargerB)
 {
     ///Ouverture et test d'ouverture du fichier
     FILE* donneJoueur = fopen("fichiersSauvegarde/joueurA.bin", "rb");
@@ -92,8 +99,9 @@ void chargerJoueur(joueur_t* joueurAcharger)
         return 1;
     }
 
-    ///Char
-    fread(joueurAcharger, sizeof(joueur_t),1,donneJoueur);
+    ///Charger
+    fread(joueurAchargerA, sizeof(joueur_t),1,donneJoueur);
+    fread(joueurAchargerB, sizeof(joueur_t),1,donneJoueur);
 
     ///Fermeture du fichier
     fclose(donneJoueur);
@@ -102,10 +110,149 @@ void chargerJoueur(joueur_t* joueurAcharger)
 
 }
 
+//////////////////////////////////////////////////////////
+////////Sous programme pour saubegarder 3 de joueurs /////
+//////////////////////////////////////////////////////////
+
+void sauvegardeTroisJoueur(joueur_t joueurA, joueur_t joueurB, joueur_t joueurC)
+{
+    ///Ouverture et test d'ouverture du fichier
+    FILE* donneJoueur = fopen("fichiersSauvegarde/joueurA.bin", "wb+");
+    if (donneJoueur == NULL)
+    {
+        printf("Erreur d'ouverture de fichier.");
+        return 1;
+    }
+
+    fwrite(&joueurA, sizeof(joueur_t),1,donneJoueur);
+    fwrite(&joueurB, sizeof(joueur_t),1,donneJoueur);
+    fwrite(&joueurC, sizeof(joueur_t),1,donneJoueur);
+
+    ///Fermeture du fichier
+    fclose(donneJoueur);
+    donneJoueur = NULL;
+}
+
+//////////////////////////////////////////////////////////
+////////Sous programme pour charger 3 de joueurs /////////
+//////////////////////////////////////////////////////////
+void chargerTroisJoueur(joueur_t* joueurAchargerA, joueur_t* joueurAchargerB, joueur_t* joueurAchargerC)
+{
+    ///Ouverture et test d'ouverture du fichier
+    FILE* donneJoueur = fopen("fichiersSauvegarde/joueurA.bin", "rb");
+    if (donneJoueur == NULL)
+    {
+        printf("Erreur d'ouverture de fichier.");
+        return 1;
+    }
+
+    ///Charger
+    fread(joueurAchargerA, sizeof(joueur_t),1,donneJoueur);
+    fread(joueurAchargerB, sizeof(joueur_t),1,donneJoueur);
+    fread(joueurAchargerC, sizeof(joueur_t),1,donneJoueur);
+
+    ///Fermeture du fichier
+    fclose(donneJoueur);
+    donneJoueur = NULL;
+
+
+}
+
+//////////////////////////////////////////////////////////
+////////Sous programme pour saubegarder 4 de joueurs /////
+//////////////////////////////////////////////////////////
+
+void sauvegardeQuatreJoueur(joueur_t joueurA, joueur_t joueurB, joueur_t joueurC, joueur_t joueurD)
+{
+    ///Ouverture et test d'ouverture du fichier
+    FILE* donneJoueur = fopen("fichiersSauvegarde/joueurA.bin", "wb+");
+    if (donneJoueur == NULL)
+    {
+        printf("Erreur d'ouverture de fichier.");
+        return 1;
+    }
+
+    fwrite(&joueurA, sizeof(joueur_t),1,donneJoueur);
+    fwrite(&joueurB, sizeof(joueur_t),1,donneJoueur);
+    fwrite(&joueurC, sizeof(joueur_t),1,donneJoueur);
+    fwrite(&joueurD, sizeof(joueur_t),1,donneJoueur);
+
+    ///Fermeture du fichier
+    fclose(donneJoueur);
+    donneJoueur = NULL;
+}
+
+//////////////////////////////////////////////////////////
+////////Sous programme pour charger 4 de joueurs /////////
+//////////////////////////////////////////////////////////
+void chargerQuatreJoueur(joueur_t* joueurAchargerA, joueur_t* joueurAchargerB, joueur_t* joueurAchargerC, joueur_t* joueurAchargerD )
+{
+    ///Ouverture et test d'ouverture du fichier
+    FILE* donneJoueur = fopen("fichiersSauvegarde/joueurA.bin", "rb");
+    if (donneJoueur == NULL)
+    {
+        printf("Erreur d'ouverture de fichier.");
+        return 1;
+    }
+
+    ///Charger
+    fread(joueurAchargerA, sizeof(joueur_t),1,donneJoueur);
+    fread(joueurAchargerB, sizeof(joueur_t),1,donneJoueur);
+    fread(joueurAchargerC, sizeof(joueur_t),1,donneJoueur);
+    fread(joueurAchargerD, sizeof(joueur_t),1,donneJoueur);
+
+    ///Fermeture du fichier
+    fclose(donneJoueur);
+    donneJoueur = NULL;
+
+
+}
+
+//////////////////////////////////////////////////////////
+/////////////Sous programme sauvegarde global ////////////
+//////////////////////////////////////////////////////////
+void sauvegardeGlobale(int nombreDeJoueurAjouer,joueur_t premierJoueur, joueur_t secondJoueur, joueur_t troisiemeJoueur, joueur_t quatriemeJoueur)
+{
+    if(nombreDeJoueurAjouer == 2)
+    {
+        sauvegardeDeuxJoueur(premierJoueur, secondJoueur);
+    }
+    if(nombreDeJoueurAjouer == 3)
+    {
+        sauvegardeTroisJoueur(premierJoueur, secondJoueur, troisiemeJoueur);
+    }
+    if(nombreDeJoueurAjouer == 4)
+    {
+        sauvegardeQuatreJoueur(premierJoueur, secondJoueur, troisiemeJoueur, quatriemeJoueur);
+    }
+}
+
+//////////////////////////////////////////////////////////
+/////////////Sous programme chargement global ////////////
+//////////////////////////////////////////////////////////
+void chargerGlobale(int nombreDeJoueurACharger,joueur_t* joueurAchargerE, joueur_t* joueurAchargerF, joueur_t* joueurAchargerG, joueur_t* joueurAchargerH)
+{
+    if(nombreDeJoueurACharger == 2)
+    {
+        chargerDeuxJoueur(&joueurAchargerE, &joueurAchargerF);
+    }
+    if(nombreDeJoueurACharger == 3)
+    {
+        chargerTroisJoueur(&joueurAchargerE, &joueurAchargerF, &joueurAchargerG);
+    }
+    if(nombreDeJoueurACharger == 4)
+    {
+        chargerQuatreJoueur(&joueurAchargerE, &joueurAchargerF, &joueurAchargerG, &joueurAchargerH);
+    }
+}
 
 int main()
 {
     joueur_t joueur1;
+    joueur_t joueur2;
+    joueur_t joueur3;
+    joueur_t joueur4;
+
     strcpy(joueur1.nomJoueur,"Mathieu");
     for (int i = 0; i<24; i++)
     {
@@ -122,12 +269,65 @@ int main()
     joueur1.nbProprietefam4 = 8;
     joueur1.doubleOuNon = 19;
 
-    int nmrJoueur;
+    strcpy(joueur2.nomJoueur,"Thibault");
+    for (int i = 0; i<24; i++)
+    {
+        joueur2.possessions[i] = 2;
+    }
+    joueur2.position = 6;
+    joueur2.argent = 1600;
+    joueur2.tourPrison = 1;
+    joueur2.numeroJoueur = 4;
+    joueur2.libertePrison = 5;
+    joueur2.nbProprietefam1 = 3;
+    joueur2.nbProprietefam2 = 7;
+    joueur2.nbProprietefam3= 8;
+    joueur2.nbProprietefam4 = 9;
+    joueur2.doubleOuNon = 20;
 
-    joueur_t joueur2;
+    strcpy(joueur3.nomJoueur,"Adrien");
+    for (int i = 0; i<24; i++)
+    {
+        joueur3.possessions[i] = 3;
+    }
+    joueur3.position = 7;
+    joueur3.argent = 2000;
+    joueur3.tourPrison = 2;
+    joueur3.numeroJoueur = 5;
+    joueur3.libertePrison = 6;
+    joueur3.nbProprietefam1 = 4;
+    joueur3.nbProprietefam2 = 8;
+    joueur3.nbProprietefam3= 9;
+    joueur3.nbProprietefam4 = 10;
+    joueur3.doubleOuNon = 21;
 
-    chargerNombreJoueur(&nmrJoueur);
-    printf("%d",nmrJoueur);
+    strcpy(joueur4.nomJoueur,"Philippe");
+    for (int i = 0; i<24; i++)
+    {
+        joueur4.possessions[i] = 4;
+    }
+    joueur4.position = 8;
+    joueur4.argent = 2500;
+    joueur4.tourPrison = 3;
+    joueur4.numeroJoueur = 6;
+    joueur4.libertePrison = 7;
+    joueur4.nbProprietefam1 = 8;
+    joueur4.nbProprietefam2 = 9;
+    joueur4.nbProprietefam3= 10;
+    joueur4.nbProprietefam4 = 11;
+    joueur4.doubleOuNon = 22;
+
+    int nmrJoueur = 2;
+
+    joueur_t joueur5;
+    joueur_t joueur6;
+    joueur_t joueur7;
+    joueur_t joueur8;
+
+    sauvegardeQuatreJoueur(joueur1, joueur2, joueur3, joueur4);
+    chargerQuatreJoueur(&joueur5, &joueur6, &joueur7, &joueur8);
+    printf("Le nom du premier joueur est : %s\nLe nom du second joueur est : %s\nLe nom du troisieme joueur est : %s\nLe nom du quatrieme joueur est %s",joueur5.nomJoueur, joueur6.nomJoueur, joueur7.nomJoueur, joueur8.nomJoueur);
+
 
 
     return 0;
