5b6399b7be76681b976ede91d1ecb8bf11214a87
Adrien <41503016+BambiCube@users.noreply.github.com>
2021-12-29 09:47:13 +0100

    Update header.h

--- CodeFinalProjetMonopoly/header.h
+++ CodeFinalProjetMonopoly/header.h
@@ -1,6 +1,9 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
+void Color(int couleurDuTexte,int couleurDeFond);
 void intro();
+void fullscreen();
+void gotoligcol( int lig, int col );
 
 #endif // HEADER_H_INCLUDED
